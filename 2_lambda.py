9d8d440869726f7d725ef27744854e9a9e9916cf
Ishwari Kape <99877551+IshwariK117@users.noreply.github.com>
2024-01-28 08:08:15 -0800

    lambda.py
    perform arithmetic operation using lambda

---
+++ 2_lambda.py
@@ -0,0 +1,22 @@
+# lambda is cstomizable variable ..we can do operation in different scenario
+# perform all arithmetic operation
+
+multiply = lambda a, b: a * b
+result = multiply(2, 3)
+print(result)
+
+add = lambda a, b: a + b
+result = add(2, 3)
+print(result)
+
+subtract = lambda a, b: a - b
+result = subtract(3, 2)
+print(result)
+
+div = lambda a, b: a / b
+result = div(10, 5)
+print(result)
+
+rem = lambda a, b: a % b
+result = rem(10, 5)
+print(result)
